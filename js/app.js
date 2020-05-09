3be12f2905b2f05b37a54060e9705ba55a870d27
Melanie <melanie_foeldes@gmx.de>
2020-05-09 09:51:11 +0200

    add dynamic navbar

--- js/app.js
+++ js/app.js
@@ -34,6 +34,22 @@
 */
 
 // build the nav
+// get number of sections
+// for each section make a new list item and append it to the element with the id 'navbar__list'
+const buildNavigationForAllSections = (event) => {
+    const sections = document.querySelectorAll("section");
+    const navbar = document.getElementById("navbar__list");
+    if (navbar && sections && sections.length > 0) {
+        console.log('in if-clause');
+        sections.forEach((value) => {
+            const sectionListElement = document.createElement("li");
+            sectionListElement.innerText = value.dataset.nav;
+            sectionListElement.classList.add("menu__link");
+            console.log(sectionListElement.innerText);
+            navbar.appendChild(sectionListElement);
+        })
+    }
+};
 
 
 // Add class 'active' to section when near top of viewport
@@ -49,6 +65,10 @@
 */
 
 // Build menu 
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM ready')
+    buildNavigationForAllSections(event);
+});
 
 // Scroll to section on link click
 
